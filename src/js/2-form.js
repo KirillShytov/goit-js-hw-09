c51d4eabe0c9e4ca9dcb9bb2327f80fd0dccde82
KirillShytov <sitovk587@gmail.com>
2024-01-07 19:35:51 +0200

    ссс

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,27 @@
 const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
-const textarea = form.elements.message;
+const textarea = form.querySelector('textarea');
+
+form.addEventListener('submit', onFormSubmit);
+textarea.addEventListener('input', onTextareaInput);
+
+populateTextarea();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+}
+
+function onTextareaInput(event) {
+  const message = event.target.value;
+  console.log(message);
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function populateTextarea() {
+  const savedMassage = localStorage.getItem(STORAGE_KEY);
+  if (savedMassage) {
+    textarea.value = savedMassage;
+  }
+}
